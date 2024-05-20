4d8a6b3891ba6c046056f828957a2e8f882a12fa
미뿌감 <80188808+20hyukim@users.noreply.github.com>
2024-05-20 21:25:06 +0900

    feat: chatroom entity

---
+++ src/main/java/com/sparta/webfluxchat/entity/ChatRoom.java
@@ -0,0 +1,17 @@
+package com.sparta.webfluxchat.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class ChatRoom {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+}
