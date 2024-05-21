4486735bba5de1c05f834a69f72c794fa6dff235
미뿌감 <80188808+20hyukim@users.noreply.github.com>
2024-05-21 12:36:24 +0900

    feat: chatroom number saved in html

--- src/main/java/com/sparta/webfluxchat/controller/api/RoomController.java
+++ src/main/java/com/sparta/webfluxchat/controller/api/RoomController.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -20,6 +22,12 @@ public class RoomController {
         List<ChatRoom> chatRooms = chatRoomService.chatLists();
         model.addAttribute("chatRooms", chatRooms);
         return "chatRoom";
+    }
+
+    @GetMapping("/chatroom/{roomnumber}")
+    public String chatPage(Model model, @PathVariable Long roomnumber) {
+        model.addAttribute("roomNumber", roomnumber);
+        return "index";
     }
 
 }
