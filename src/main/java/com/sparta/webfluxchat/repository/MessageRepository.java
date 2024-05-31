0f681b0668363f45c9353350e20627bcced4d3f2
미뿌감 <80188808+20hyukim@users.noreply.github.com>
2024-05-31 15:00:45 +0900

    feat: security base

--- src/main/java/com/sparta/webfluxchat/repository/MessageRepository.java
+++
@@ -1,9 +0,0 @@
-//package com.sparta.webfluxchat.repository;
-//
-//import com.sparta.webfluxchat.entity.Message;
-//import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-//import reactor.core.publisher.Flux;
-//
-//public interface MessageRepository extends ReactiveMongoRepository<Message, String> {
-//    Flux<Message> findByOrderByTimestampAsc();
-//}
